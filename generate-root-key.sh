cc32b52ed74d9aa1b2ee0dca8de078365a5b9bf2
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-10-20 22:58:06 +0900

    docs: ROOT証明書作成コマンドにヘルプを追加

--- generate-root-key.sh
+++ generate-root-key.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+set -e
+
+display_usage() {
+    echo "Usage: $0 [ROOT_CERT_COMMON_NAME] [ROOT_CERT_LIFETIME_DAYS]"
+    echo 'Generate root certificate'
+    echo ''
+    echo '  ROOT_CERT_COMMON_NAME   : (default) "Private CA"'
+    echo '  ROOT_CERT_LIFETIME_DAYS : (default) 30 days'
+}
+
+if [[ $1 == -* ]] || [[ $1 == help ]] ; then
+    display_usage
+    exit 1
+fi
+
 set -ex
 
 # 第一引数を渡した場合、ROOT証明書の「発行先」・「発行者」を変更できる
